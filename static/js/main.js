e1d069ad219c885230d284797cc00545a4cb3e1c
Arqam Siddiqui <akkosidd@gmail.com>
2022-12-13 23:15:32 +0500

    consultation exclamantion mark removal

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 Calendly.initBadgeWidget({
   url: "https://calendly.com/minal-ahsan",
-  text: "Book your meeting now!",
+  text: "Book your meeting now",
   color: "#E38601",
   textColor: "#ffffff",
   branding: false,
