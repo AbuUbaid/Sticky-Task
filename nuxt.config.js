e62c022955ee44359c4f50307dfb188afaf843f5
Arqam Siddiqui <akkosidd@gmail.com>
2022-12-19 18:06:53 +0500

    Added gtm

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,15 @@ export default {
         src: "//js-na1.hs-scripts.com/21981248.js",
         defer: true,
       },
+      {
+        hid: "gtm",
+        children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WJSV6KD');`,
+        type: "text/javascript",
+      },
     ],
   },
 
