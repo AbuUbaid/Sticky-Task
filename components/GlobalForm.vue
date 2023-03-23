76ecbec5fc5d5f80899336290e1e716bee010891
Ubaid kahn <124548952+abuubaid12@users.noreply.github.com>
2023-03-23 15:44:51 +0500

    form changs

--- components/GlobalForm.vue
+++ components/GlobalForm.vue
@@ -20,8 +20,8 @@
 	 <input type='text' style='display:none;' id='ldeskuid' name='ldeskuid'></input>
 	 <input type='text' style='display:none;' id='LDTuvid' name='LDTuvid'></input>
 	 <!-- Do not remove this code. -->
-<div class='zcwf_title' style='max-width: 600px;color: black;'>Contact Us ATS</div>
-<div class='zcwf_row'><div class='zcwf_col_lab' ><label for='LEADCF1'>Technology</label></div><div class='zcwf_col_fld'><select class='zcwf_col_fld_slt' id='LEADCF1' name='LEADCF1'  >
+<!-- <div class='zcwf_title' style='max-width: 600px;color: black;'>Contact Us ATS</div> -->
+<div class='zcwf_row'><div class='zcwf_col_lab' ><label for='LEADCF1'>How can we help you?<span style='color:red;'>*</span></label></div><div class='zcwf_col_fld'><select class='zcwf_col_fld_slt' id='LEADCF1' name='LEADCF1'  >
 			<option value='-None-'></option>
 			<option value='Mobile&#x20;App&#x20;Development'>Mobile App Development</option>
 			<option value='Enterprise&#x20;Software&#x20;Development'>Enterprise Software Development</option>
@@ -36,10 +36,14 @@
 			<option value='FinTech'>FinTech</option>
 			<option value='Other'>Other</option>
 		</select><div class='zcwf_col_help'></div></div></div>
-<div class='zcwf_row'><div class='zcwf_col_lab' ><label for='Last_Name'>Last Name<span style='color:red;'>*</span></label></div><div class='zcwf_col_fld'><input type='text' id='Last_Name' name='Last Name' maxlength='80'></input><div class='zcwf_col_help'></div></div></div>
-<div class='zcwf_row'><div class='zcwf_col_lab' ><label for='Email'>Email</label></div><div class='zcwf_col_fld'><input type='text' ftype='email' id='Email' name='Email' maxlength='100'></input><div class='zcwf_col_help'></div></div></div>
-<div class='zcwf_row'><div class='zcwf_col_lab' ><label for='Phone'>Phone</label></div><div class='zcwf_col_fld'><input type='text' id='Phone' name='Phone' maxlength='30'></input><div class='zcwf_col_help'></div></div></div>
-<div class='zcwf_row'><div class='zcwf_col_lab' ><label for='LEADCF5'>Organization</label></div><div class='zcwf_col_fld'><input type='text' id='LEADCF5' name='LEADCF5' maxlength='255'></input><div class='zcwf_col_help'></div></div></div>
+    <div class="forst-rwo">
+      <div class='zcwf_row'><div class='zcwf_col_lab' ><label for='Last_Name'>Name<span style='color:red;'>*</span></label></div><div class='zcwf_col_fld'><input type='text' id='Last_Name' name='Last Name' maxlength='80'></input><div class='zcwf_col_help'></div></div></div>
+      <div class='zcwf_row'><div class='zcwf_col_lab' ><label for='Email'>Email<span style='color:red;'>*</span></label></div><div class='zcwf_col_fld'><input type='text' ftype='email' id='Email' name='Email' maxlength='100'></input><div class='zcwf_col_help'></div></div></div>
+    </div>
+    <div class="first-rwo">
+      <div class='zcwf_row'><div class='zcwf_col_lab' ><label for='Phone'>Phone</label></div><div class='zcwf_col_fld'><input type='text' id='Phone' name='Phone' maxlength='30'></input><div class='zcwf_col_help'></div></div></div>
+      <div class='zcwf_row'><div class='zcwf_col_lab' ><label for='LEADCF5'>Organization</label></div><div class='zcwf_col_fld'><input type='text' id='LEADCF5' name='LEADCF5' maxlength='255'></input><div class='zcwf_col_help'></div></div></div>
+    </div>
 <div class='zcwf_row wfrm_fld_dpNn'><div class='zcwf_col_lab' ><label for='Lead_Source'>Lead Source</label></div><div class='zcwf_col_fld'><select class='zcwf_col_fld_slt' id='Lead_Source' name='Lead Source'  >
 			<option value='-None-'>-None-</option>
 			<option value='Advertisement'>Advertisement</option>
@@ -73,8 +77,10 @@
 <div style="display: none;" class='zcwf_row'><div class='zcwf_col_lab' ><label for='LEADCF2'>IP</label></div><div class='zcwf_col_fld'><input type='text' id='LEADCF2' name='LEADCF2' maxlength='255'></input><div class='zcwf_col_help'></div></div></div>
 <div style="display: none;" class='zcwf_row'><div class='zcwf_col_lab' ><label for='LEADCF4'>IP to ISP</label></div><div class='zcwf_col_fld'><input type='text' id='LEADCF4' name='LEADCF4' maxlength='255'></input><div class='zcwf_col_help'></div></div></div>
 <div style="display: none;" class='zcwf_row'><div class='zcwf_col_lab' ><label for='LEADCF3'>IP to Org</label></div><div class='zcwf_col_fld'><input type='text' id='LEADCF3' name='LEADCF3' maxlength='255'></input>
-<div class='zcwf_col_help'></div></div></div><div class='zcwf_row'> <div class='zcwf_col_lab'></div><div class='zcwf_col_fld'><div class='g-recaptcha' data-sitekey='6LeifxglAAAAAMimqECX74kJLz2dKwOfpHMaqSp1' data-theme='light' data-callback='rccallback5666867000000732007' captcha-verified='false' id='recap5666867000000732007'></div><div  id='recapErr5666867000000732007' style='font-size:12px;color:red;visibility:hidden;'>Captcha validation failed. If you are not a robot then please try again.</div></div></div><div class='zcwf_row'><div class='zcwf_col_lab'></div><div class='zcwf_col_fld'><input type='submit' id='formsubmit' class='formsubmit zcwf_button' value='Submit' title='Submit'><input type='reset' class='zcwf_button' name='reset' value='Reset' title='Reset'></div></div>
-	<script>
+<div class='zcwf_col_help'></div></div></div><div class='zcwf_row'> <div class='zcwf_col_lab'></div><div class='zcwf_col_fld'><div class='g-recaptcha' data-sitekey='6LeifxglAAAAAMimqECX74kJLz2dKwOfpHMaqSp1' data-theme='light' data-callback='rccallback5666867000000732007' captcha-verified='false' id='recap5666867000000732007'></div><div  id='recapErr5666867000000732007' style='font-size:12px;color:red;visibility:hidden;'>Captcha validation failed. If you are not a robot then please try again.</div></div></div><div class='zcwf_row'><div class='zcwf_col_lab'></div><div class='zcwf_col_fld'><input type='submit' id='formsubmit' class='formsubmit zcwf_button' value='Send Message' title='Submit'><input type='reset' class='zcwf_button' name='reset' value='Reset' title='Reset'></div></div>
+
+
+<script>
 
   /* Do not remove this code. */
  	function rccallback5666867000000732007()
@@ -175,8 +181,12 @@ function tooltipShow5666867000000732007(el){
 		tooltip.style.display='none';
 	}
 }
-</script><script type='text/javascript' id='VisitorTracking'>var $zoho= $zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:'738cfd870777f809baa64c9149c46ae907c4701df07d176a80adba46865c6d8e8bc5c201f0e5d1a9486f9dbf87f44a7d', values:{},ready:function(){}};var d=document;s=d.createElement('script');s.type='text/javascript';s.id='zsiqscript';s.defer=true;s.src='https://salesiq.zoho.com/widget';t=d.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);function trackVisitor5666867000000732007(){try{if($zoho){var LDTuvidObj = document.forms['WebToLeads5666867000000732007']['LDTuvid'];if(LDTuvidObj){LDTuvidObj.value = $zoho.salesiq.visitor.uniqueid();}var firstnameObj = document.forms['WebToLeads5666867000000732007']['First Name'];if(firstnameObj){name = firstnameObj.value +' '+name;}$zoho.salesiq.visitor.name(name);var emailObj = document.forms['WebToLeads5666867000000732007']['Email'];if(emailObj){email = emailObj.value;$zoho.salesiq.visitor.email(email);}}} catch(e){}}</script>
-	<!-- Do not remove this --- Analytics Tracking code starts --><script id='wf_anal' src='https://crm.zohopublic.com/crm/WebFormAnalyticsServeServlet?rid=31df86b79df9162e83ac89da5ca807f7ee555de13b9464e3c8dd7315a8c64bfcgid83b0d711022caae66bd1d22ffb265a0813ad5d65632006f83c3c16e37766bdb9gid885e3c1045bd9bdcc91bdf30f82b5696gid14f4ec16431e0686150daa43f3210513&tw=61690b96c1d0471b638f31426f38e68aa67fb7ed6da86f32dc10ad817fe55a0a'></script><!-- Do not remove this --- Analytics Tracking code ends. --></form>
+</script>
+<script type='text/javascript' id='VisitorTracking'>var $zoho= $zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:'738cfd870777f809baa64c9149c46ae907c4701df07d176a80adba46865c6d8e8bc5c201f0e5d1a9486f9dbf87f44a7d', values:{},ready:function(){}};var d=document;s=d.createElement('script');s.type='text/javascript';s.id='zsiqscript';s.defer=true;s.src='https://salesiq.zoho.com/widget';t=d.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);function trackVisitor5666867000000732007(){try{if($zoho){var LDTuvidObj = document.forms['WebToLeads5666867000000732007']['LDTuvid'];if(LDTuvidObj){LDTuvidObj.value = $zoho.salesiq.visitor.uniqueid();}var firstnameObj = document.forms['WebToLeads5666867000000732007']['First Name'];if(firstnameObj){name = firstnameObj.value +' '+name;}$zoho.salesiq.visitor.name(name);var emailObj = document.forms['WebToLeads5666867000000732007']['Email'];if(emailObj){email = emailObj.value;$zoho.salesiq.visitor.email(email);}}} catch(e){}}</script>
+	<!-- Do not remove this --- Analytics Tracking code starts -->
+  <script id='wf_anal' src='https://crm.zohopublic.com/crm/WebFormAnalyticsServeServlet?rid=31df86b79df9162e83ac89da5ca807f7ee555de13b9464e3c8dd7315a8c64bfcgid83b0d711022caae66bd1d22ffb265a0813ad5d65632006f83c3c16e37766bdb9gid885e3c1045bd9bdcc91bdf30f82b5696gid14f4ec16431e0686150daa43f3210513&tw=61690b96c1d0471b638f31426f38e68aa67fb7ed6da86f32dc10ad817fe55a0a'></script>
+  <!-- Do not remove this --- Analytics Tracking code ends. -->
+</form>
 </div>
       </div>
       <div class="badges-container">
